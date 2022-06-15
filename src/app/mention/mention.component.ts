2881d0b77b7555a983d97128b83cff8205f88f8d
Thibaud ARNAULT <thibaud.arnault@smartpush.fr>
2022-06-15 23:36:05 +0200

    add mention + fix mobile

---
+++ src/app/mention/mention.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './mention.component.html',
+  styleUrls: ['./mention.component.scss']
+})
+export class MentionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
